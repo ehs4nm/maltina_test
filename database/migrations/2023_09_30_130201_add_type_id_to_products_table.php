af76f682d7e69738e9ea1de6d9146a64ee7c7832
ehs4nm <64464913+ehs4nm@users.noreply.github.com>
2023-09-30 17:53:08 +0330

    add type_id to products table for the relation

---
+++ database/migrations/2023_09_30_130201_add_type_id_to_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Type;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // Add the foreign key column
+            $table->foreignIdFor(Type::class)->nullable();
+            // Define the foreign key constraint
+            $table->foreign('type_id')->references('id')->on('types')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // Remove the foreign key column
+            $table->dropForeignIdFor(Type::class);
+
+            // Drop the column
+            $table->dropColumn('type_id');
+        });
+    }
+};
