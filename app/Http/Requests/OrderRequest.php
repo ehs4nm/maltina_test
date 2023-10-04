dd70616e0fe81962f471888f7ad0e4bbd0b7d8b0
ehs4nm <64464913+ehs4nm@users.noreply.github.com>
2023-10-04 15:25:01 +0330

    modify the requeired field for orders

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -23,9 +23,13 @@ class OrderRequest extends FormRequest
     {
         return [
             'user_id' => 'nullable|exists:users,id', // Ensure the user_id exists in the 'users' table.
-            'total_price' => 'nullable|integer|min:0|max:999999',
-            'status' => 'nullable|in:WAITING,PREPARATION,READY,DELIVERED',
+            // 'total_price' => 'nullable|integer|min:0|max:999999', // user should not set the total price, it should be calculated by app
+            // 'status' => 'nullable|in:WAITING,PREPARATION,READY,DELIVERED', // user should not set the status, it should be calculated by app
             'consume_location' => 'required|in:TAKE_AWAY,IN_SHOP',
+            'products' => 'required|array', // Ensure 'products' is an array
+            'products.*.product_id' => 'required|exists:products,id', // Validate each product_id
+            'products.*.option_id' => 'required|exists:options,id', // Validate each option_id
+            'products.*.quantity' => 'required|integer|min:1|max:100', // Validate quantity for each product
         ];
     }
 }
