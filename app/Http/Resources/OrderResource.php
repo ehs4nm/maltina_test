a3c190c028a3412a413adfc232193457ceec9c29
ehs4nm <64464913+ehs4nm@users.noreply.github.com>
2023-10-04 15:24:49 +0330

    modify OrderResource for a better response

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -12,6 +12,18 @@ class OrderResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+    private function transformProducts($products)
+    {
+        return $products->map(function ($product) {
+            return [
+                'product_id' => $product->pivot->product_id,
+                'quantity' => $product->pivot->quantity,
+                'sum_price' => $product->pivot->sum_price,
+                'option_id' => $product->pivot->option_id,
+            ];
+        });
+    }
+
     public function toArray(Request $request): array
     {
         return [
@@ -19,7 +31,9 @@ class OrderResource extends JsonResource
             'status' => $this->status,
             'total_price' => $this->total_price,
             'consume_location' => $this->consume_location,
-            'user_id' => $this->user_id, // it should be hidded too, not letting the consumer know the id for users
+            'user_id' => $this->user_id, // it should be hidden too, not letting the consumer know the id for users
+            'products' => $this->transformProducts($this->cart->products),
+            // 'products' => ProductResource::collection($this->whenLoaded('cart.products')),
         ];
     }
 }
